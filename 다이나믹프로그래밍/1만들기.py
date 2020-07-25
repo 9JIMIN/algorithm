10845181d2f52fc76d1ca411a0753dd1382b2c55
9jimin <909jimin@gmail.com>
2020-07-25 22:30:53 +0900

    2n타일링, 1로 만들기

--- 다이나믹프로그래밍/1만들기.py
+++ 다이나믹프로그래밍/1만들기.py
@@ -14,29 +14,12 @@ X가 2로 나누어 떨어지면, 2로 나눈다.
 이런 문제는 처음이라 접근을 아예 잘 못했다. 이제 알았으니, 다음부턴 잘 하자.
  """
 
-N = int(input())
-
-result = [0 for _ in range(N + 1)]
-
-for i in range(1, N + 1):
-    if i == 1:
-        result[i] = 0
-        continue
-    result[i] = result[i-1] + 1
-    if i % 3 == 0 and result[i//3] + 1 < result[i]:
-        result[i] = result[i//3] + 1
-    if i % 2 == 0 and result[i//2] + 1< result[i]:
-        result[i] = result[i//2] + 1
-        
-print(result[N])
-
 """ 
 2
  """
 def f(n):
     if n <= 2: return n-1
     return 1 + min(f(n//3)+n%3, f(n//2)+n%2)
-
 print(f(int(input())))
 
 """ 
