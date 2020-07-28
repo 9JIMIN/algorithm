3932eaf3b2bdc9da80cd864dbaee1cae423339c9
9jimin <909jimin@gmail.com>
2020-07-28 11:20:08 +0900

    1,2,3더하기5, 쉬운계단수

---
+++ 다이나믹프로그래밍/쉬운계단수.py
@@ -0,0 +1,16 @@
+num = [[0 for _ in range(10)] for _ in range(101)]
+
+for i in range(1, 101):
+    for j in range(10):
+        if i == 1:
+            num[i][j] = 1
+        else:
+            if 1 <= j <= 8:
+                num[i][j] = num[i-1][j-1] + num[i-1][j+1]
+            elif j == 0:
+                num[i][j] = num[i-1][j+1]
+            elif j == 9:
+                num[i][j] = num[i-1][j-11]
+                
+N = int(input())             
+print(sum(num[N][1:10]) % 1000000000)
