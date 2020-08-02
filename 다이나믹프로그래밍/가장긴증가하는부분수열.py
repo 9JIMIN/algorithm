a8a092458d5d19b1621a6d53b8642734eac3277a
9jimin <909jimin@gmail.com>
2020-08-03 00:36:48 +0900

    정리

--- 다이나믹프로그래밍/가장긴증가하는부분수열.py
+++ 다이나믹프로그래밍/가장긴증가하는부분수열.py
@@ -1,6 +1,26 @@
-input()
-num = map(int, input().split())
+# input()
+# num = map(int, input().split())
+# d = {}
+# for n in num:
+#     d[sum( n > d[k] for k in d )] = n
+# print(len(d))
+
+# n = int(input())
+# num = list(map(int, input().split()))
+# d = [0]*n
+
+# for i in range(n):
+#     for j in range(i):
+#         if num[i] > num[j] and d[i] < d[j]:
+#             d[i] = d[j]
+#     d[i] += 1
+
+# print(max(d)) 
+
+n = int(input())
+num = list(map(int, input().split()))
 d = {}
-for n in num:
-    d[sum( n > d[k] for k in d )] = n
+
+for x in num:
+    d[sum(x > d[i] for i in d)] = x
 print(len(d))
--- 다이나믹프로그래밍/합분해.py
+++ 다이나믹프로그래밍/합분해.py
@@ -1,18 +1,9 @@
-# N, K = map(int, input().split())
-
-# def f(n, k):
-#     if k == 1: return 1
-#     if k == 2: return n+1
-#     return sum([i*f(n+1-i, k-2) for i in range(1, n+2)])%1000000000
-
-# print(f(N, K))
-
 n, k = map(int, input().split())
-mod = 1000000000
-dp = [[0]*(n+1) for _ in range(k+1)]
-dp[0][0] = 1
-for i in range(1, k+1):
-    for j in range(n+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        dp[i][j] %= mod
-print(dp[k][n])
+
+d = [[1]*(n+1) for _ in range(k)]
+
+for i in range(1, k):
+    for j in range(1, n+1):
+        d[i][j] = (d[i-1][j] + d[i][j-1])%1000000000
+
+print(d[k-1][n])
